fd89aa43385b8d66e73849107e23da063de95c24
Renlin Liu <liurenlin@abukito.com>
2021-12-13 19:26:03 +0800

    servlet

--- src/main/java/pers/maxlcoder/servlet/HelloServlet.java
+++ src/main/java/pers/maxlcoder/servlet/HelloServlet.java
@@ -1,15 +1,15 @@
 package pers.maxlcoder.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-
-@WebServlet(urlPatterns = "/hello")
+@WebServlet(urlPatterns = "/")
 public class HelloServlet extends HttpServlet {
 
     @Override
@@ -20,7 +20,7 @@ public class HelloServlet extends HttpServlet {
             name = "world";
         }
         PrintWriter pw = resp.getWriter();
-        pw.write("<h1>Hello. " + name + "!</h1>");
+        pw.write("<h1>Hello, " + name + "!</h1>");
         pw.flush();
     }
 }
--- src/main/java/pers/maxlcoder/servlet/Main.java
+++ src/main/java/pers/maxlcoder/servlet/Main.java
@@ -1,18 +1,18 @@
 package pers.maxlcoder.servlet;
 
+import java.io.File;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
-import java.io.File;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.getInteger("port", 8088));
+        tomcat.setPort(Integer.getInteger("port", 8085));
         tomcat.getConnector();
         Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
         WebResourceRoot resources = new StandardRoot(ctx);
@@ -23,3 +23,4 @@ public class Main {
         tomcat.getServer().await();
     }
 }
+
